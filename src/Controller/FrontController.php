5c6185487cb14934c7c41c74a781afb945e7aa6d
Joanna Filip <joanne.filip@gmail.com>
2021-04-18 18:18:35 +0200

    avancement layout

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -8,9 +8,34 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class FrontController extends AbstractController
 {
-    #[Route('/front/index')]
+    #[Route('/front/index', name: 'index')]
     public function index(): Response
     {
         return $this->render('front/index.html.twig');
+    }
+    #[Route('/front/produits', name: 'produits')]
+    public function produits(): Response
+    {
+        return $this->render('front/produits.html.twig');
+    }
+    #[Route('/front/producteurs', name: 'producteurs')]
+    public function producteurs(): Response
+    {
+        return $this->render('front/producteurs.html.twig');
+    }
+    #[Route('/front/apropos', name: 'apropos')]
+    public function apropos(): Response
+    {
+        return $this->render('front/apropos.html.twig');
+    }
+    #[Route('/front/login', name: 'login')]
+    public function login(): Response
+    {
+        return $this->render('front/login.html.twig');
+    }
+    #[Route('/front/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('front/contact.html.twig');
     }
 }
