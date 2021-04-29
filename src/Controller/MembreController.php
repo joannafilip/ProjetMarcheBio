d9826679b54895d98c082765868a14c54cc63a82
Joanna Filip <joanne.filip@gmail.com>
2021-04-29 19:56:33 +0200

    Ajout page inscription producteur

--- src/Controller/MembreController.php
+++ src/Controller/MembreController.php
@@ -13,10 +13,20 @@ class MembreController extends AbstractController
     {
         return $this->render('membre/login.html.twig');
     }
+    #[Route('/membre/accueil/inscription', name: 'accueilInscription')]
+    public function accueilInscription(): Response
+    {
+        return $this->render('membre/accueilInscription.html.twig');
+    }
     #[Route('/membre/inscription/client', name: 'inscriptionClient')]
     public function inscription(): Response
     {
         return $this->render('membre/inscriptionClient.html.twig');
+    }
+    #[Route('/membre/inscription/producteur', name: 'inscriptionProducteur')]
+    public function inscriptionProducteur(): Response
+    {
+        return $this->render('membre/inscriptionProducteur.html.twig');
     }
     #[Route('/membre/profil', name: 'profil')]
     public function profil(): Response
