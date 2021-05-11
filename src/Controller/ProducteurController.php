2a6c24c9a737b52400df2f5f093b671b3832492a
Joanna Filip <joanne.filip@gmail.com>
2021-05-11 17:00:27 +0200

    Page produits publiés ok.

---
+++ src/Controller/ProducteurController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
+
+class ProducteurController extends AbstractController
+{
+    #[Route('/producteur', name: 'produitsPublies')]
+    public function produitsPublies(): Response
+    {
+        $this->getUser();
+        // $publications = $repository->getUser();
+        // $vars = ['publications' => $publications];
+        return $this->render('producteur/produitsPublies.html.twig');
+    }
+}
